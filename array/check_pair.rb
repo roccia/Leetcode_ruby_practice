36066fb56de3340d3230004cdf807a3e50b88d46
roccia <lzzy.ywz@gmail.com>
2018-03-18 22:08:35 +0800

    two sum

--- array/check_pair.rb
+++ array/check_pair.rb
@@ -5,14 +5,40 @@ class CheckPair
   def check_pair(ary,target)
        return 0 if ary.size <= 1
        hash = Hash.new
-       (ary.size-1).downto(0) do |i|
+       res = []
+       (0..ary.size-1).each do |i|
            temp = target - ary[i]
-           if temp > 0 && hash[temp] == 1
-             p  "#{ary[i]} and #{temp} "
+           if hash.has_key?(temp)
+             p hash[temp]
+             res << [temp,ary[i]]
            end
          hash[ary[i]] = 1
        end
+    res
   end
+
+
+  #Two Sum II - Input array is sorted
+  def check_pair_index(nums,target)
+    (nums.size-1).downto(0).each do |i|
+         left = i+1
+         right = nums.size - 1
+         temp = target - nums[i]
+        while left <= right
+             mid = (left+right)/2
+             if nums[mid] == temp
+                 return [i+1,mid+1]
+             elsif nums[mid] < temp
+                 left = mid + 1
+               p "left : #{left}"
+             else
+                right = mid - 1
+               p right
+             end
+        end
+      end
+  end
+
 
   def check_pair_dp(ary, target)
     return 0 if ary.size < 1
@@ -34,5 +60,5 @@ end
 
 
 c = CheckPair.new
-p ary = [1, 2, 3, 4, 5, 6]
-p c.check_pair(ary, 6)
+p ary = [1,2,3,4,5]
+p c.check_pair_index(ary, 6)
