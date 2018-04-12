f2c865efe037a9d5e14f8e696a1dda3e63915da6
roccia <lzzy.ywz@gmail.com>
2018-04-12 16:10:36 +0800

    fix

---
+++ array/minimum_size_subarray_sum.rb
@@ -0,0 +1,45 @@
+class MinimumSizeSubarraySum
+  def min_sub_array_len(s, nums)
+    return 0 if nums.nil? || nums.empty?
+    first = 0
+    last = 0
+    sum = 0
+    result = nums.length+1
+
+    while last < nums.length
+      while sum < s && last < nums.length
+        sum += nums[last]
+        last += 1
+      end
+
+      while sum >= s && first < last
+        result = [last - first, result].min
+        sum -= nums[first]
+        first += 1
+      end
+    end
+
+    result == nums.length+1 ? 0 : result
+
+  end
+
+  def min_sub_array_len_2(s, nums)
+    start, minlen = 0, nums.size + 1
+    sum = 0
+    (0...nums.size).each do |i|
+      sum += nums[i]
+      while sum >= s
+        minlen = [minlen, i - start + 1].min
+        sum -= nums[start]
+        start+=1
+      end
+    end
+
+    minlen == nums.size+1 ? 0 : minlen
+  end
+
+end
+
+
+m = MinimumSizeSubarraySum.new
+p m.min_sub_array_len(7, [2, 3, 1, 2, 4, 3])
